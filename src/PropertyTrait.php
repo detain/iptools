753cbfa61a82176e691e54bd3468779a57b9f331
Joe Huss <detain@interserver.net>
2020-04-17 20:03:32 -0400

    spacing updtes

--- src/PropertyTrait.php
+++ src/PropertyTrait.php
@@ -7,39 +7,39 @@ namespace IPTools;
  */
 trait PropertyTrait
 {
-	/**
-	 * @param string $name
-	 * @return mixed
-	 */
-	public function __get($name)
-	{
-		if(method_exists($this, $name)) {
-			return $this->$name();
-		}
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if(method_exists($this, $name)) {
+            return $this->$name();
+        }
 
-		foreach (array('get', 'to') as $prefix) {
+        foreach (array('get', 'to') as $prefix) {
             $method = $prefix . ucfirst($name);
             if(method_exists($this, $method)) {
                 return $this->$method();
             }
         }
 
-		trigger_error('Undefined property');
-		return null;
-	}
+        trigger_error('Undefined property');
+        return null;
+    }
 
-	/**
-	 * @param string $name
-	 * @param mixed $value
-	 */
-	public function __set($name, $value)
-	{
-		$method = 'set'. ucfirst($name);
-		if (!method_exists($this, $method)) {
-			trigger_error('Undefined property');
-			return;
-		}
-		$this->$method($value);
-	}
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set'. ucfirst($name);
+        if (!method_exists($this, $method)) {
+            trigger_error('Undefined property');
+            return;
+        }
+        $this->$method($value);
+    }
 
 }
